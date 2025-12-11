28769d7f9a28a5fee7cb462a3380567f2cfdcaae
umamahesh7991 <umamaheshkanakala@gmail.com>
2025-12-12 01:01:38 +0530

    obj.js

--- obj.js
+++ obj.js
@@ -7,3 +7,22 @@ document.write(object.emp_id);
 document.write("------------");
 delete object.salary;
 document.write(object.salary);
+var address={
+    d_no:13-24-3,
+    location: {
+        street:"bhanoji colony",
+        city:"visakhapatnam",
+        state:"andhra pradesh"
+    },
+    pincode:530026,
+    wish:function f1(){
+        document.write("Welcome to home town");
+    },
+};
+//  var fcall=address.wish();
+var fcall=address["wish"]();
+var f1=address["location"]["city"];
+// document.write(address.location.city);
+document.write(f1);
+// document.write("------------ ");
+// document.write(fcall);
